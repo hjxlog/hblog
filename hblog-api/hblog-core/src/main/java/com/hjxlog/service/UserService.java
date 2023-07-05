1ec14803658af426e165ca6ba85eba3ff181b85f
HuangJX <hjxlog@gmail.com>
2023-07-05 08:23:12 +0800

    【hblog-api】生成用户模块代码

---
+++ hblog-api/hblog-core/src/main/java/com/hjxlog/service/UserService.java
@@ -0,0 +1,13 @@
+package com.hjxlog.service;
+
+import com.hjxlog.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author hjx
+* @description 针对表【user】的数据库操作Service
+* @createDate 2023-07-04 23:52:10
+*/
+public interface UserService extends IService<User> {
+
+}
