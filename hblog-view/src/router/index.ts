41c12fb407fb2d6c2418c8d890857d26560e3912
HuangJX <hjxlog@gmail.com>
2024-01-11 08:15:43 +0800

    【hblog-view】 1、修改登陆样式 2、路由改成history模式

--- hblog-view/src/router/index.ts
+++ hblog-view/src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import {ElMessage} from "element-plus";
 
 const routes: Array<RouteRecordRaw> = [
@@ -160,7 +160,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
+    history: createWebHistory(process.env.BASE_URL),
     routes
 })
 
